b3c82ee4426d27d029b8385e3a67784e478b9d95
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2022-09-16 21:11:25 +0530

    some changes before deployement

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -20,15 +20,9 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Shorya Agarwal" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        {/* <meta name="twitter:title" content="Takuya Matsuyama" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Shorya Agarwal" />
+        <meta name="og:title" content="Shorya Agarwal" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
         <title>Shorya Agarwal - Homepage</title>
       </Head>
 
